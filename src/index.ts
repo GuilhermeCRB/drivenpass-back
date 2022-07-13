0a8c24ac6955064ecf2971b9cb2cab7c0a0b0275
GuilhermeCRB <guilhermecrbarros@gmail.com>
2022-07-13 16:32:49 -0300

    feat: creates error handler middleware

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import chalk from "chalk";
 
 import router from "./routers";
+import { errorHandlerMiddleware } from "./middlewares/errorHandlerMiddleware";
 
 dotenv.config();
 
@@ -12,6 +13,7 @@ const app = express();
 app.use(json());
 app.use(cors());
 app.use(router);
+app.use(errorHandlerMiddleware);
 
 const port = +process.env.PORT || 5000;
 app.listen(port, () => {
