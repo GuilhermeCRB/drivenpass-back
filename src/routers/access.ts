019c595bca2d4b1b1c631a691f6ce3e8941d052f
GuilhermeCRB <guilhermecrbarros@gmail.com>
2022-07-14 17:38:48 -0300

    feat: validate inputs

--- src/routers/access.ts
+++ src/routers/access.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
 import { signUp } from "../controllers/accessController.js";
+import { validateSchema } from "../middlewares/validateSchema.js";
+import userSchema from "../schemas/userSchema.js";
 
 const access = Router();
 
-access.post("/sign-up", signUp);
+access.post("/sign-up", validateSchema(userSchema), signUp);
 
 export default access;
