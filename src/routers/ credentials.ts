a940c2ba34bf8c26b18690f5cb876b3fb7807748
GuilhermeCRB <guilhermecrbarros@gmail.com>
2022-07-15 12:07:12 -0300

    feat: creates token validation

--- src/routers/ credentials.ts
+++ src/routers/ credentials.ts
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import validateToken from "../middlewares/validateToken.js";
 
 const credentials = Router();
+
+credentials.post("/credentials", validateToken);
 
 export default credentials;
